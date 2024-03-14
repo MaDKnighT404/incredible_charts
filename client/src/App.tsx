786c0e10063f2095c191d8c737d3970edc7cd944
George Koloidi <Battleaxe@bk.ru>
2024-03-14 22:44:30 +0100

    init: start new project

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import '@/App.css';
+import { createTheme } from '@mui/material/styles';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+
+function App() {
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+		<div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+		</div>
+	);
+}
+
+export default App;
